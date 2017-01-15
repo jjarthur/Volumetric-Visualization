c4f29fb94f54a03749ae30f2a68f04fef5d451bc
James Arthur <jarthur96@hotmail.com>
2017-01-16 09:34:54 +1100

    Added rounding to statistics values

--- Scripts/SetValueText.cs
+++ Scripts/SetValueText.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,30 +10,32 @@ public class SetValueText : MonoBehaviour {
 
     public void SetValue(float value)
     {
+        double newValue = Math.Round(Convert.ToDouble(value), 2);
+
         //If this object is DownscaleValueText
         if (gameObject.name.Equals("DownscaleValueText"))
         {
             //Add multiplier
-            gameObject.GetComponent<Text>().text = value.ToString() + "X";
+            gameObject.GetComponent<Text>().text = newValue.ToString() + "X";
         }
         //If this object is OpacityValueText
         else if (gameObject.name.Equals("OpacityValueText"))
         {
             //Convert to percentage
-            value *= 100;
-            gameObject.GetComponent<Text>().text = value.ToString() + "%";
+            newValue *= 100;
+            gameObject.GetComponent<Text>().text = newValue.ToString() + "%";
         }
         //If this object is DepthBlendValueText
         else if (gameObject.name.Equals("DepthBlendValueText"))
         {
             //Convert to percentage
-            value *= 50;
-            gameObject.GetComponent<Text>().text = value.ToString() + "%";
+            newValue *= 50;
+            gameObject.GetComponent<Text>().text = newValue.ToString() + "%";
         }
         //If this object is X, Y or Z slice coordinate
         else
         {
-            gameObject.GetComponent<Text>().text = value.ToString();
+            gameObject.GetComponent<Text>().text = newValue.ToString();
         }
     }
 }
